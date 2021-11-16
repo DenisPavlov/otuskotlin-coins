a57da715b952cb7d3850768f378705ec9965e50d
Denis Pavlov <denispavlov1989@gmail.com>
2021-11-17 00:08:56 +0700

    init parent gradle project with subproject

---
+++ hello-world/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.6.0"
+}
+
+group = "com.example"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "otuskotlin-coins"
+include("hello-world")
